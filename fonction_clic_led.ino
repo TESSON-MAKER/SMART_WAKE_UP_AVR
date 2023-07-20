f7622acbd7a48489455a949b6a4e267f673bb851
TESSON-MAKER <85193015+TESSON-MAKER@users.noreply.github.com>
2023-07-20 22:00:58 +0200

    Add files via upload
    This Arduino code controls a set of LEDs and buttons. It uses two arrays, `LED` and `BTN`, to define the pins to which the LEDs and buttons are connected. The total number of LEDs is determined by `NumLED`, and the total number of buttons is determined by `NumBTN`. There is also an array `lastButtonState` to store the previous state of each button for detecting state changes.
    
    In the `setup()` function, the pins of the LEDs are configured as outputs and initialized to a brightness of 0, while the pins of the buttons are configured as inputs.
    
    The main program is in the `loop()` function. It uses a custom function `clic(int num)` to detect clicks on the first button (Button 22, `BTN[0]`). When the button is clicked, the corresponding LED (LED on pin 2, `LED[0]`) is turned on if it is off, or turned off if it is already on. A 1-second delay is then added to avoid button bounce.
    
    The `clic(int num)` function is used to detect clicks on a specific button, where `num` is the index of the button in the `BTN` array. It reads the current state of the button, checks if the state has changed since the last reading, and adds a debounce delay to avoid false triggers. If the button is pressed and it is not a false trigger (bounce), the function returns `true`, otherwise, it returns `false`.
    
    In summary, this Arduino code allows controlling an LED using a button. Each click on Button 22 (BTN[0]) turns the LED connected to pin 2 (LED[0]) on or off. The code takes into account button bounce to avoid unwanted actions. The use of a non-blocking timing mechanism using `millis()` allows detecting clicks while still allowing the program to respond to other events.

---
+++ fonction_clic_led.ino
@@ -0,0 +1,76 @@
+const int LED[] = {2, 3, 4, 5, 6};
+const int BTN[] = {22, 23, 24, 25, 26};
+
+const int NumLED = sizeof(LED) / sizeof(LED[0]);
+const int NumBTN = sizeof(BTN) / sizeof(BTN[0]);
+
+int lastButtonState[NumBTN] = {LOW}; // Tableau pour stocker l'etat precedent de chaque bouton
+const int debounceDelay = 50;        // Delai en millisecondes pour le temps de rebond
+
+
+////////////////////////////////////////SETUP////////////////////////////////////////////
+void setup()
+{
+  for (int i = 0; i < NumLED; i++)
+  {
+    pinMode(LED[i], OUTPUT); // Configure les broches des LED en sortie
+  }
+
+  for (int i = 0; i < NumBTN; i++)
+  {
+    pinMode(BTN[i], INPUT); // Configure les broches des boutons en entree
+  }
+}
+
+////////////////////////////////////////LOOP/////////////////////////////////////////////
+
+void loop()
+{
+  static bool ledOn = false; // Variable pour stocker l'état de la LED (allumée ou éteinte)
+
+  if (clic(0)) // Vérifie si le bouton 22 a été cliqué (le tableau commence à l'index 0)
+  {
+    if (!ledOn) // Si la LED est éteinte
+    {
+      digitalWrite(LED[0], HIGH); // Allume la LED à la pin 2 (correspondant à LED[0])
+      ledOn = true; // Met à jour l'état de la LED
+    }
+    else // Si la LED est allumée
+    {
+      digitalWrite(LED[0], LOW); // Éteint la LED
+      ledOn = false; // Met à jour l'état de la LED
+    }
+
+    delay(1000); // Attend une seconde pour éviter les rebonds du bouton
+  }
+}
+
+////////////////////////////////////////Fonctions////////////////////////////////////////
+
+
+/*---------------------------------------Clic---------------------------------------*/
+
+bool clic(int num)
+{
+  int buttonState = digitalRead(BTN[num]);  // Lecture de l'etat du bouton correspondant au numero
+  if (buttonState != lastButtonState[num])
+  {
+    delay(debounceDelay);                   // Delai de rebond pour eviter les faux declenchements
+    buttonState = digitalRead(BTN[num]);    // Lecture de l'etat du bouton a nouveau
+    if (buttonState == HIGH)
+    {
+      lastButtonState[num] = buttonState;   // Met a jour l'etat precedent du bouton
+      return true;                          // Renvoie vrai si le bouton est presse
+    }
+  }
+  lastButtonState[num] = buttonState; // Met a jour l'etat precedent du bouton
+  return false;                       // Renvoie faux si le bouton n'est pas presse
+}
+
+
+
+
+
+
+
+
